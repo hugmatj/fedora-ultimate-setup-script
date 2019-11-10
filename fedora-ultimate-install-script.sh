853508c065a3c99a7448d4f73b2429b1efffe93d
David <12832280+David-Else@users.noreply.github.com>
2019-11-10 17:59:00 +0000

    Update prettier VS Code extension

--- fedora-ultimate-install-script.sh
+++ fedora-ultimate-install-script.sh
@@ -127,7 +127,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
         bierner.markdown-preview-github-styles
         bmewburn.vscode-intelephense-client
         deerawan.vscode-dash
-        esbenp.prettier-vscode
+        prettier.prettier-vscode
         foxundermoon.shell-format
         msjsdiag.debugger-for-chrome
         ritwickdey.LiveServer
