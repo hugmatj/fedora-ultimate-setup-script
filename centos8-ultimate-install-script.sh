e2bf554b85acd1ae13c2fbe4447aa651b7c4ee3a
David <12832280+David-Else@users.noreply.github.com>
2020-10-30 12:48:37 +0000

    Add GitHub CLI to developer_packages

--- centos8-ultimate-install-script.sh
+++ centos8-ultimate-install-script.sh
@@ -93,6 +93,7 @@ if [[ $webdev =~ ^[Yy]$ ]]; then
     # packages for web development option * deno added if selected
     #==========================================================================
     developer_packages=(
+        gh
         code
         php
         gcc-c++
@@ -156,6 +157,9 @@ case " ${packages_to_install[*]} " in
 *' code '*)
     rpm --import https://packages.microsoft.com/keys/microsoft.asc
     sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+    ;;&
+*' gh '*)
+    dnf -y config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
     ;;&
 *' brave-browser '*)
     dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
