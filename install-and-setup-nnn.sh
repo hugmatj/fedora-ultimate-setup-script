156e93d5b539e722dc71ee919dabf1533ffa9af0
David <12832280+David-Else@users.noreply.github.com>
2021-04-08 17:46:18 +0100

    Add plugins and fix typo

--- install-and-setup-nnn.sh
+++ install-and-setup-nnn.sh
@@ -3,13 +3,13 @@
 curl -LOf https://github.com/jarun/nnn/releases/download/v3.5/nnn-3.5-1.el8.0.centos.x86_64.rpm
 chmod +x ./nnn-3.5-1.el8.0.centos.x86_64.rpm
 sudo dnf install ./nnn-3.5-1.el8.0.centos.x86_64.rpm
+curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | sh
 
-mkdir -p "$HOME/.config/nnn/plugins"
+# https://raw.githubusercontent.com/mpv-player/mpv/master/TOOLS/umpv -o "$HOME/Documents/scripts/umpv"
 
 cat >"$HOME/.config/nnn/plugins/addtoplaylist" <<'EOL'
 #!/usr/bin/env sh
-# Open selected files in MPV playlist
-curl https://raw.githubusercontent.com/mpv-player/mpv/master/TOOLS/umpv -o "$HOME/Documents/scripts/umpv"
+# Open selected files in MPV playlist curl
 
 player="umpv"
 selection=${NNN_SEL:-${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.selection}
@@ -21,10 +21,12 @@ fi
 EOL
 
 chmod +x "$HOME/.config/nnn/plugins/addtoplaylist"
+
 cat >>"$HOME/.bashrc" <<'EOL'
 export NNN_PLUG="p:addtoplaylist"
 export NNN_BMS='d:~/Documents;D:~/Downloads;p:~/Pictures;v:~/Videos;m:~/Music;h:~/'
 export NNN_TRASH=1
 
-alias nnn="nnn -e"
+# -e open text in $VISUAL/$EDITOR/vi, -x selection to system clipboard
+alias nnn="nnn -xe"
 EOL
