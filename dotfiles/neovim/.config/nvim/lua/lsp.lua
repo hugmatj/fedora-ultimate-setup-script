9d78e5e1456735f39bd4807a5e288ebea9695232
David <12832280+David-Else@users.noreply.github.com>
2021-05-03 11:51:42 +0100

    Update LSP config based on latest changes in original

--- dotfiles/neovim/.config/nvim/lua/lsp.lua
+++ dotfiles/neovim/.config/nvim/lua/lsp.lua
@@ -1,7 +1,4 @@
--- taken from https://github.com/neovim/nvim-lspconfig#keybindings-and-completion
--- changed servers and ctermbg=237
--- added buf_set_keymap('n', 'ga', '<Cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-
+-- modified from https://github.com/neovim/nvim-lspconfig#keybindings-and-completion 3rd May 2021
 local nvim_lsp = require('lspconfig')
 local on_attach = function(client, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
@@ -11,7 +8,6 @@ local on_attach = function(client, bufnr)
 
   -- Mappings.
   local opts = { noremap=true, silent=true }
-  buf_set_keymap('n', 'ga', '<Cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
   buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
   buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
@@ -22,6 +18,7 @@ local on_attach = function(client, bufnr)
   buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
   buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
   buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
   buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
   buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
   buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
@@ -31,16 +28,18 @@ local on_attach = function(client, bufnr)
   -- Set some keybinds conditional on server capabilities
   if client.resolved_capabilities.document_formatting then
     buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-  elseif client.resolved_capabilities.document_range_formatting then
-    buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+  end
+  if client.resolved_capabilities.document_range_formatting then
+    buf_set_keymap("v", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
   end
 
   -- Set autocommands conditional on server_capabilities
+  -- Hightlight color taken from nvcode hi CursorLine value
   if client.resolved_capabilities.document_highlight then
     vim.api.nvim_exec([[
-      hi LspReferenceRead ctermbg=237 guibg=LightYellow
-      hi LspReferenceText ctermbg=237 guibg=LightYellow
-      hi LspReferenceWrite ctermbg=237 guibg=LightYellow
+      hi LspReferenceRead guibg=#2c323c ctermbg=236 
+      hi LspReferenceText guibg=#2c323c ctermbg=236
+      hi LspReferenceWrite guibg=#2c323c ctermbg=236
       augroup lsp_document_highlight
         autocmd! * <buffer>
         autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
@@ -57,7 +56,8 @@ for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup { on_attach = on_attach }
 end
 
-require'lspconfig'.denols.setup{
+-- Add the deno language server with linting enabled
+nvim_lsp.denols.setup{
   on_attach = on_attach,
   init_options = {
     lint = true,
